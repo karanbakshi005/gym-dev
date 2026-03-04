9eeabaa11e9ee8fc8d63559f10311674e57f3e29
karan <karan@arishi.agency>
2026-03-05 01:24:14 +0530

    move-to-vercel

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
@@ -43,7 +45,7 @@ const trainerData = [
       { icon: FaYoutube, href: "http://Youtube.com" },
     ],
   },
-    {
+  {
     image: "/assets/img/trainers/rosy.jpg",
     name: "Rosy",
     role: "Body builder coach",
@@ -61,23 +63,69 @@ const Team = () => {
   return (
     <section className="py-12 xl:h-[110vh]" id="team">
       <div className="container mx-auto h-full flex flex-col items-center justify-center">
-        <h2 className="h2 text-center mb-6">Our trainers</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12 mb-12">
+        <motion.h2
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="h2 text-center mb-6"
+        >
+          Our trainers
+        </motion.h2>
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12 mb-12"
+        >
           {trainerData.map((trainer, index) => {
             return (
-              <div key={index}>
+              <div
+                className="flex flex-col items-center text-center"
+                key={index}
+              >
                 <div className="relative w-[320px] h-90 mx-auto mb-4">
-                  <Image src={trainer.image} fill  alt=''/>
+                  <Image src={trainer.image} fill alt="" />
+                </div>
+                <h4 className="h4 mb-2">{trainer.name}</h4>
+                <p className="uppercase text-xs tracking-[3px] mb-2">
+                  {trainer.role}
+                </p>
+                <p className="mb-6">{trainer.description}</p>
+                <div className="flex gap-12 justify-center">
+                  {trainer.social.map((social, index) => {
+                    return (
+                      <div key={index}>
+                        <Link
+                          href={social.href}
+                          className="hover:text-brand transition-all"
+                        >
+                          {" "}
+                          <social.icon className="text-lg" />{" "}
+                        </Link>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             );
           })}
-        </div>
+        </motion.div>
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <CustomButton
+            containerStyle="w-[196px] h-[62px]"
+            text="See all trainer"
+          />
+        </motion.div>
       </div>
     </section>
   );
 };
 
 export default Team;
-
-
